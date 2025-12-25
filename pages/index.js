564b3866ce3ae2a6f5eabd81aee43415b0c05894
Kacper Mlodkowski <k.mlodkowski@gmail.com>
2025-12-25 16:00:42 +0100

    modified welcome message

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         <meta name="description" content="Welcome to Kacper's website" />
       </Head>
       <div>
-        <h1>Welcome to Kacper's website</h1>
+        <h1>Hi and welcome to my website</h1>
         <p>My first website</p>
         <p style={{ marginTop: '1rem' }}>
           This is the home page. Use the navigation bar on the left to explore other sections of the website.
